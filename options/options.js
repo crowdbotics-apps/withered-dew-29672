0c6f2184de198dd321eeb75df6c0630b37cc9bff
Crowdbotics <team@crowdbotics.com>
2021-08-09 19:57:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "withered_dew_29672Identifier",
+  url: "https://withered_dew_29672Identifier.botics.co",
+  api: "https://withered_dew_29672Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
